ca683a3b39f9f328a6c8ed687d64975835f05bee
Yingke Yu <yy2792@columbia.edu>
2022-11-25 16:23:37 -0500

    Create HomeController.java

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping()
+    public String getHome() {
+
+        return "home";
+    }
+}
