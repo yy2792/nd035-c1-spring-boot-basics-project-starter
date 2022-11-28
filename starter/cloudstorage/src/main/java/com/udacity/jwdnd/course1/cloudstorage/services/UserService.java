ae66e7205035826cdefe06662bf2be19a715e2f3
Yingke Yu <yy2792@columbia.edu>
2022-11-28 10:29:26 -0500

    Update UserService.java

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
@@ -33,5 +34,10 @@ public class UserService {
 
     public User getUser(String username) {
         return userMapper.getUser(username);
+    }
+
+    public Integer getLoggedInUserId() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUser(username).getUserId();
     }
 }
