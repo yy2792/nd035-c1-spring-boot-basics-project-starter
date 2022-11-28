ee2ecac2ce5f35a734404ce0b9187b16dfc6f280
Yingke Yu <yy2792@columbia.edu>
2022-11-28 10:08:46 -0500

    Update FileController.java

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -3,17 +3,21 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.Objects;
+
 
 @Controller
 @RequestMapping({"/file"})
@@ -22,21 +26,18 @@ public class FileController {
     private UserService userService;
     private FileService fileService;
 
+    private Logger logger = LoggerFactory.getLogger(FileController.class);
+
     public FileController(UserService userService, FileService fileService) {
         this.userService = userService;
         this.fileService = fileService;
     }
 
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) throws IOException {
+    public String uploadFile(@RequestParam("fileUpload") MultipartFile file, RedirectAttributes attributes) throws IOException {
         // check if file is empty
         if (file.isEmpty()) {
             attributes.addFlashAttribute("message", "Please select a file to upload.");
-            return "redirect:/home";
-        }
-
-        if (file.getSize() > 5 * 1024 * 1024) {
-            attributes.addFlashAttribute("warning", "Your file cannot exceed 5MB!");
             return "redirect:/home";
         }
 
@@ -48,15 +49,35 @@ public class FileController {
             return "redirect:/home";
         }
 
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String file_err = null;
 
-        Integer userId = userService.getUser(username).getUserId();
-        File fileUpload = new File(fileName, file.getContentType(), String.valueOf(file.getSize()), userId, file.getBytes());
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        fileService.insertFile(fileUpload);
+            Integer userId = userService.getUser(username).getUserId();
+            File fileUpload = new File(fileName, file.getContentType(), String.valueOf(file.getSize()), userId, file.getBytes());
 
-        attributes.addFlashAttribute("message", "You successfully uploaded " + fileName + '!');
+            fileService.insertFile(fileUpload);
+        }
+        catch (MaxUploadSizeExceededException a){
+            String errorMessage = "file size limit exceed expception captured";
+            logger.error(errorMessage);
+            logger.error(a.toString());
+            a.printStackTrace();
+            file_err = errorMessage;
+        }
+        catch (Exception a){
+            logger.error(a.toString());
+            a.printStackTrace();
+            file_err = a.toString();
+        }
 
+        if (file_err == null) {
+            attributes.addFlashAttribute("uploadsuccess", "You successfully uploaded " + fileName + '!');
+        }
+        else {
+            attributes.addFlashAttribute("uploaderror","You failed to upload " + fileName + '!');
+        }
         return "redirect:/home";
 
 
